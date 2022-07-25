28fed0f2a2852af224c3c338225a90a1a1d3b6cb
Satyam Ohlan <satyam.ohlan1998@gmail.com>
2022-07-25 22:35:32 +0530

    Initial Commit

---
+++ index.py
@@ -0,0 +1,123 @@
+import mysql.connector as sql
+from zmq import NULL
+db = sql.connect(
+    host='localhost',
+    user='admin2077',
+    password='Satyam@2005',
+    database='library'
+)
+mycursor = db.cursor()
+print('Welcome to Epsilon Book Issuing Services!')
+admin = input('Enter Admin Username: ')
+admin_password = input('Enter Admin Password: ')
+
+mycursor.execute('SELECT name,admin_id from admins WHERE admin_id="{admin}" AND password="{password}"'.format(
+    admin=admin, password=admin_password))
+app_user = mycursor.fetchone()
+if(app_user != None):
+    print('Welcome %s!' % (app_user[0]))
+    while True:
+        print('Press 1 to issue/Un-Issue a book \nPress 2 to See All-TIme/Yearly/Monthly Stats \nPress 3 to See Overdue issued books \nPress 4 to Exit')
+        act1 = int(input('Enter Your Action: '))
+        if(act1 == 1):
+            student = input('Enter ID of the Student: ')
+            mycursor.execute(
+                'SELECT * FROM student WHERE student_id=%s ' % (student))
+            issue_student = mycursor.fetchone()
+            if(issue_student != None):
+                print('Name Of Student: %s \nClass Of student: %s \nAdmission Number of Student: %s' % (
+                    issue_student[1], issue_student[2], issue_student[0]))
+                mycursor.execute(
+                    "SELECT * FROM issues WHERE student_id=%s AND returned='NO' " % (student))
+                issued_books = mycursor.fetchall()
+                if(issued_books != None):
+                    print('Books Currently Issued to Student: ')
+                    for x in issued_books:
+                        mycursor.execute(
+                            "SELECT * from books where book_id=%s" % x[2])
+                        book = mycursor.fetchone()
+                        print(book)
+                while True:
+                    print('Press 1 to Issue a Book \nPress 2 to Un-issue an Earlier Book \nPress 3 to Explore Book Catalogue by Name/Author \nPress 4 to Exit ')
+                    act2 = int(input('Enter Your Action: '))
+
+                    if(act2 == 1):
+                        mycursor.execute(
+                            "SELECT * FROM issues WHERE student_id=%s and returned='NO'" % (student))
+                        issued_books = mycursor.fetchall()
+                        if(len(issued_books) < 2):
+                            book = input(
+                                'Enter ID of the Book You Want to Issue: ')
+                            mycursor.execute(
+                                'SELECT * from books where book_id=%s' % book)
+                            issue_book = mycursor.fetchone()
+                            if(issue_book != None):
+                                print(issue_book)
+                                Final_signal = input(
+                                    'Do You Want to Issue the Following book?(Y/N) ').lower()
+                                if(Final_signal in ['y', 'yes']):
+                                    try:
+                                        mycursor.execute("INSERT INTO issues(student_id,book_id,issued_by) VALUES('%s',%s,'%s')" % (
+                                            issue_student[0], issue_book[0], app_user[1]))
+                                        db.commit()
+                                    except sql.Error as error:
+                                        print(
+                                            "Something went wrong: {}".format(error))
+                                    else:
+                                        print('Book Issued Successfully!\n')
+
+                                elif(Final_signal in ['n', 'no']):
+                                    print('OK')
+                                else:
+                                    print('Input Not Understood.')
+                            else:
+                                print('No Book With Following Code Exists')
+                        else:
+                            print(
+                                'Student Cannot have more than 2 books Issued at the same time.')
+                    elif(act2 == 2):
+                        unissue = int(
+                            input('Enter Book Code for the book to be Returned: '))
+
+                        print("Returning Book...\n")
+                        mycursor.execute("UPDATE TABLE issues SET returned='YES' where student_id='%s' and book_id='%s'" % (
+                            issue_student[0], unissue))
+                        db.commit()
+                    elif(act2 == 3):
+                        print(
+                            'Genres available: Tech, Science, Non-Fiction, Fiction, Philosophy')
+                        genre = input(
+                            'Enter the Term You want to Search/Genre You want to Explore: ').lower()
+                        print(
+                            "SELECT * FROM books WHERE genre='{genre}'or book_name like '%{genre}%';" .format(genre=genre))
+                        mycursor.execute(
+                            "SELECT * FROM books WHERE genre='{genre}'or book_name like '%{genre}%';" .format(genre=genre))
+                        list = mycursor.fetchall()
+                        print('\n'.join('{}: {}'.format(*val)
+                                        for val in enumerate(list)))
+                    elif(act2 == 4):
+                        print('Exiting...\n')
+                        break
+            else:
+                print('No Student with Following Admission Number')
+        elif(act1 == 3):
+            mycursor.execute(
+                "SELECT * FROM issues where returned!='YES' AND TIMESPAN>NOW()")
+            overdue_books = mycursor.fetchall()
+            for x in overdue_books:
+                mycursor.execute(
+                    "SELECT * from books where book_id=%s" % x[2])
+                overdue_book = mycursor.fetchone()
+                mycursor.execute(
+                    "SELECT * from student where student_id=%s" % x[1])
+                overdue_student = mycursor.fetchone()
+                print(overdue_book)
+                print('Issue ID:{ID}    Issued To:{Student}     Issued By:{Admin}       Overdue Since:{Date}'.format(
+                    ID=x[0], Student=str(overdue_student[1])+' '+str(overdue_student[2]), Admin=x[4], Date=x[5]))
+
+        elif act1 == 4:
+            print('Thank You for Visiting Us!')
+            break
+
+else:
+    print('Wrong Password/Username Entered')
---
+++ init.py
@@ -0,0 +1,64 @@
+import mysql.connector as sql
+import csv
+db = sql.connect(
+    host='localhost',
+    user='admin2077',
+    password='Satyam@2005',
+    database='library'
+)
+mycursor = db.cursor(buffered=True)
+books = []
+# mycursor.execute('CREATE DATABASE library')
+# mycursor.execute('DROP TABLE admins')
+# mycursor.execute('DROP TABLE student')
+# mycursor.execute('DROP TABLE books')
+# mycursor.execute('DROP TABLE issues')
+
+
+# mycursor.execute(
+# 'CREATE TABLE admins(admin_id varchar(30) unique NOT NULL,name  VARCHAR(20) NOT NULL,password varchar(30) NOT NULL);')
+# mycursor.execute(
+# 'CREATE TABLE student(student_id varchar(30) unique NOT NULL,name  VARCHAR(20) NOT NULL ,class_no smallint UNSIGNED NOT NULL, PRIMARY KEY(student_id));')
+
+# mycursor.execute(
+#     'CREATE TABLE books(book_id INT auto_increment,book_name  VARCHAR(60) NOT NULL,genre varchar(20) default "other",sub_genre varchar(100),author varchar(30),book_length SMALLINT UNSIGNED, PRIMARY KEY(book_id));')
+
+# mycursor.execute(
+# 'CREATE TABLE issues(issue_id INT auto_increment,student_id varchar(30) NOT NULL,FOREIGN KEY(student_id) REFERENCES student(student_id),book_id INT unique NOT NULL,FOREIGN KEY(book_id) REFERENCES books(book_id),issued_on DATETIME NOT NULL default now(),issued_by varchar(30) NOT NULL,FOREIGN KEY(issued_by) REFERENCES admins(admin_id),PRIMARY KEY(issue_id));')
+
+
+# mycursor.execute('DESCRIBE issues')
+# for x in mycursor:
+#     print(x)
+# y = int(input("Enter Number oF Admins to be registered"))
+# for x in range(y):
+#     id = input('Enter the ID for user {no}: '.format(no=x+1))
+#     name = input('Enter the Name for user {no}: '.format(no=x+1))
+#     password = input('Enter the Password for user {no}: '.format(no=x+1))
+#     mycursor.execute(
+#         'INSERT INTO admins VALUES("{id}","{name}","{password}");'.format(id=id, name=name, password=password))
+# mycursor.execute('select * from admins')
+# for x in mycursor:
+#     print(x)
+# y = int(input("Enter Number oF Students to be registered"))
+# for x in range(y):
+#     id = input('Enter the ID for user {no}: '.format(no=x+1))
+#     name = input('Enter the Name for user {no}: '.format(no=x+1))
+#     class_no = int(input('Enter the Class for user {no}: '.format(no=x+1)))
+#     mycursor.execute(
+#         'INSERT INTO student(student_id,name,class_no) VALUES("{id}","{name}",{class_no});'.format(id=id, name=name, class_no=class_no))
+# mycursor.execute('select * from student')
+# for x in mycursor:
+#     print(x)
+
+# with open('books_new.csv', 'r') as f1:
+#     fileReader = csv.reader(f1)
+#     next(fileReader)
+#     for row in fileReader:
+#         mycursor.execute(
+#             'INSERT INTO books(book_name,genre,sub_genre,author,book_length) VALUES("{name}","{genre}","{subgenre}","{author}","{book_length}");'.format(name=row[0], genre=row[2], subgenre=row[3], author=row[1], book_length=row[4]))
+# mycursor.execute('select * from books')
+# for x in mycursor:
+#     print(x)
+
+db.commit()
